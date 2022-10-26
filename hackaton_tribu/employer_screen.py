1ff72c84c1299fdf5175150768f8ab14bd2d8652
levimichelle <levimichelle999@gmail.com>
2022-10-26 15:22:10 +0300

    changes

--- hackaton_tribu/employer_screen.py
+++ hackaton_tribu/employer_screen.py
@@ -1,16 +1,16 @@
 import tkinter
 
 def enter_place_name():
-    return place_name = input()
+    place_name = input()
 
 def enter_place_location():
-    return place_location = [radiobuttons]
+    place_location = [radiobuttons]
 
 def enter_place_hobby():
-    return place_hobby = [radiobuttons]
+    place_hobby = [radiobuttons]
 
 def enter_place_info():
-    return place_info = [str]
+    place_info = [str]
 
 def closing_screen():
     print("Your place has been submitted! \n Thank you for your contribution!")
